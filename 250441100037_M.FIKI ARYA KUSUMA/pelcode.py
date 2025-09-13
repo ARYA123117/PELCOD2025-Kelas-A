4f4ac075f29b8ca0c6b3987e1c862b5e9f1504f0
Fiki Arya <september2022arya@gmail.com>
2025-09-13 11:30:29 +0700

    250441100037_M.FikiAryaKusuma

---
+++ 250441100037_M.FIKI ARYA KUSUMA/pelcode.py
@@ -0,0 +1,38 @@
+# print ("="*50)
+# print ("nama saya fiki arya")
+# print ("="*50)
+# nama = "fiki arya"
+# print (nama)
+
+# boolean = True
+# print(type(boolean))
+
+# penjumlahan = 5+7
+# print ("hasilnya adalah", penjumlahan)
+# nama_asprak = "arya medan"
+# nama_calonDosen =nama_asprak
+# print (nama_calonDosen)
+
+# tipe data number
+# tahunLahir = int (input ("masukan tahun lahir : "))
+# tahunSekarang =int (input ("masukan tahun sekarang : "))
+# penghitunganUsia = tahunSekarang - tahunLahir
+# print ("Usia saya adalah", penghitunganUsia)
+
+# bilangan = 51
+# bilangan2 = 7
+
+ # jumlah = int (bilangan) + int(bilangan2)
+# print (jumlah)
+
+ # cek = int (bilangan) < int(bilangan2)
+ # print (cek)
+
+# modulus = bilangan % bilangan2
+# print (modulus)
+
+saldo = float (input("masukan saldo awal :"))
+deposit = float(input ("masukan saldo deposit : "))
+
+saldo += deposit
+print ("hasil saldonya adalah" , saldo)
